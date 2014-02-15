94217e1341416b877400bbd99765929b81fbeaca
Patrick Kelso <patrick@teamkelso.local>
2014-02-16 07:28:33 +1100

    my first app typo correction

--- headfirst/myfirstapp.java
+++ headfirst/myfirstapp.java
@@ -1,6 +1,6 @@
 public class myfirstapp {
 
-	public static void main (string[] args) {
+	public static void main (String[] args) {
 		System.out.println("I Rule!");
 		System.out.println("The World");
 	}
