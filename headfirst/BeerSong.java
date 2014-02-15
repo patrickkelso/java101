6f8097203ce26dc9f1029ce6e6b73b10c2e923a0
Patrick Kelso <patrick@teamkelso.local>
2014-02-16 07:57:14 +1100

    more typo

--- headfirst/BeerSong.java
+++ headfirst/BeerSong.java
@@ -8,13 +8,13 @@ public class BeerSong {
         word = "bottle";
       }
     System.out.println(beernum + " " + word + " of beer on the wall");
-    System.out.println(beernum + " " word + " of beer");
+    System.out.println(beernum + " " + word + " of beer");
     System.out.println("Take one down.");
     System.out.println("Pass it around.");
     beernum = beernum -1;
 
     if (beernum > 0) {
-      System.out.println(beernum + " " word + " of beer on the wall");
+      System.out.println(beernum + " " + word + " of beer on the wall");
     } else {
       System.out.println("No more bottles of beer on the wall");
     }
