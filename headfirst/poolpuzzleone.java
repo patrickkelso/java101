6bf58a236bbd2424d8fcd0127add13f58124873f
Patrick Kelso <patrick@teamkelso.local>
2014-02-16 09:20:17 +1100

    Headfirst Java pg 24

---
+++ headfirst/poolpuzzleone.java
@@ -0,0 +1,26 @@
+class poolpuzzleone {
+  public static void main(String [] args) {
+  int x = 0;
+  while (x < 4 ) {
+    System.out.print("a");
+    if ( x < 1 ) {
+      System.out.print(" ");
+    }
+      System.out.print("n");
+    if ( x > 1 ) {
+      System.out.print(" oyster");
+      x = x + 1;
+    }
+    if ( x == 1 ) {
+      System.out.print("oise")
+    }
+    if ( x > 3 ) {
+      System.out.print("annoys")
+    }
+    System.out.println("");
+    x = x + 2;
+  }
+}
+}
+
+    
