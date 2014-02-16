325d9aeaf6ef9f0b5c334306c9f5a74771691a2c
Patrick Kelso <patrick@teamkelso.local>
2014-02-16 11:01:47 +1100

    headfirst pg43

---
+++ headfirst/drumkit.java
@@ -0,0 +1,20 @@
+class DrumKit {
+  boolean tophat = true;
+  boolean snare = true;
+  void playsnare() {
+    System.out.println("bang bang ba-bang");
+  }
+  void playtophat () {
+    System.out.println("ding ding da-ding");
+  }
+}  
+class DrumKitTestDrive {
+  public static void main(String [] args) {
+  Drumkit d = new DrumKit();
+  if (d.snare == true {
+    d.playsnare();
+  }
+  d.snare = false;
+  d.playtophat();
+  }
+} 
