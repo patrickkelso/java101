d13b7aecf73e5b71f92c168b5926af5223da48cd
Patrick K <patrick@teamkelso.org>
2014-03-05 18:45:35 +1100

    inputs

--- FOJ/week2/HouseSize.java
+++ FOJ/week2/HouseSize.java
@@ -1,9 +1,18 @@
+import java.util.*;
+
 public class HouseSize {
-  public static void main (String[] args) {
-    int houselength = 10;
-    int housewidth = 5;
-    int househeight = 3;
-    double roofheight = 2.5;
+   public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("What is the house length");
+    int houselength = keyboard.nextInt();
+    System.out.print("What is the house width");
+    int housewidth = keyboard.nextInt();
+    System.out.print("What is the house height");
+    int househeight = keyboard.nextInt();
+    System.out.print("What is the roof height");
+    double roofheight = keyboard.nextDouble();
     int housevolume = houselength * housewidth * househeight;
     double roofvolume = .5 * houselength * housewidth * roofheight;
     double totalvolume = housevolume + roofvolume;
