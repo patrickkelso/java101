06199f1f70e19b506cee328e692e22150e4e2658
Patrick Kelso <patrick@teamkelso.local>
2014-03-05 16:46:11 +1100

    Cubing it yo

---
+++ FOJ/week1/HouseSize.java
@@ -0,0 +1,10 @@
+public class HouseSize {
+  public static void main (String[] args) {
+    int houselength = 10;
+    int housewidth = 5;
+    int househeight = 3;
+    double roofheight = 2.5;
+    int housesize = houselength * housewidth * househeight;
+    System.out.println("The house size is " + housesize + "m cubed");
+ }
+}
