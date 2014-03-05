f9b864736c4050f228f6ef4425e321ba3e6fa201
Patrick K <patrick@teamkelso.org>
2014-03-05 19:24:11 +1100

    reverse order, what the a newline

--- FOJ/week2/Age.java
+++ FOJ/week2/Age.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Age {
   public static void main (String[] args) {
     Scanner keyboard = new Scanner(System.in);
+    System.out.print("Please enter your age: ");
+    int age = keyboard.nextInt();
+    keyboard.nextLine();
     System.out.print("Please enter your name: ");
     String name = keyboard.nextLine();
-    System.out.print("Please enter your age " + name + ": ");
-    int age = keyboard.nextInt();
     System.out.println("Hello " + name + " you are " + age + " years old.");
  }
 }
