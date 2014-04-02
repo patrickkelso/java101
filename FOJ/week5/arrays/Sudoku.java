d3af81c03a51ea503c935f413fcfcfeff8ebc8c2
Patrick K <patrick@teamkelso.org>
2014-04-02 18:31:55 +1100

    refactoring

--- FOJ/week5/arrays/Sudoku.java
+++ FOJ/week5/arrays/Sudoku.java
@@ -8,34 +8,34 @@
 public class Sudoku
 {
     // The sudoku board has 4 regions.
-    private Group[] reg;
+    private Group[] regs;
     // 4 rows
-    private Group[] row;
+    private Group[] rows;
     // and 4 columns
-    private Group[] col;
+    private Group[] cols;
 
     public Sudoku()
     {
         // If you use an array here, you can also easily use an array loop.
-        reg = new Group[4];
-        reg[0] = new Group();
-        reg[1] = new Group();
-        reg[2] = new Group();
-        reg[3] = new Group();
+        regs = new Group[4];
+        regs[0] = new Group();
+        regs[1] = new Group();
+        regs[2] = new Group();
+        regs[3] = new Group();
 
         // You can use arrays here, but don't bother using a loop - it is too difficult.
-        row = new Group[4];
-        row[0] = new Group(reg[0], reg[1], 1, 2);
-        row[1] = new Group(reg[0], reg[1], 3, 4);
-        row[2] = new Group(reg[2], reg[3], 1, 2);
-        row[3] = new Group(reg[2], reg[3], 3, 4);
+        rows = new Group[4];
+        rows[0] = new Group(regs[0], regs[1], 1, 2);
+        rows[1] = new Group(regs[0], regs[1], 3, 4);
+        rows[2] = new Group(regs[2], regs[3], 1, 2);
+        rows[3] = new Group(regs[2], regs[3], 3, 4);
 
         // You can use arrays here, but don't bother using a loop.
-        col = new Group[4];
-        col[0] = new Group(reg[0], reg[2], 1, 3);
-        col[1] = new Group(reg[0], reg[2], 2, 4);
-        col[2] = new Group(reg[1], reg[3], 1, 3);
-        col[3] = new Group(reg[1], reg[3], 2, 4);
+        cols = new Group[4];
+        cols[0] = new Group(regs[0], regs[2], 1, 3);
+        cols[1] = new Group(regs[0], regs[2], 2, 4);
+        cols[2] = new Group(regs[1], regs[3], 1, 3);
+        cols[3] = new Group(regs[1], regs[3], 2, 4);
     }
 
     public void enterNumber(int region, int cell, int number, boolean lock)
@@ -46,14 +46,15 @@ public class Sudoku
     // NOTE! The parameter will be in the range 1-4, not 0-3.
     private Group getRegion(int regionNumber)
     {
-        switch (regionNumber)
-        {
-            case 1: return reg[0];
-            case 2: return reg[1];
-            case 3: return reg[2];
-            case 4: return reg[3];
-        }
-        return null;
+      return regs[regionNumber-1];
+    //    switch (regionNumber)
+    //    {
+    //        case 1: return regs[0];
+    //        case 2: return regs[1];
+    //        case 3: return regs[2];
+    //        case 4: return regs[3];
+    //    }
+    //    return null;
     }
 
     /**
@@ -62,18 +63,18 @@ public class Sudoku
      */
     public boolean isSolved()
     {
-        return reg[0].isSolved() && reg[1].isSolved() && reg[2].isSolved() && reg[3].isSolved()
-            && row[0].isSolved() && row[1].isSolved() && row[2].isSolved() && row[3].isSolved()
-            && col[0].isSolved() && col[1].isSolved() && col[2].isSolved() && col[3].isSolved();
+        return regs[0].isSolved() && regs[1].isSolved() && regs[2].isSolved() && regs[3].isSolved()
+            && rows[0].isSolved() && rows[1].isSolved() && rows[2].isSolved() && rows[3].isSolved()
+            && cols[0].isSolved() && cols[1].isSolved() && cols[2].isSolved() && cols[3].isSolved();
     }
 
     public void print()
     {
         System.out.println();
-        row[0].print();
-        row[1].print();
-        row[2].print();
-        row[3].print();
+        rows[0].print();
+        rows[1].print();
+        rows[2].print();
+        rows[3].print();
         System.out.println();
     }
 }
