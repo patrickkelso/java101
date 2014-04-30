4c2928e335dde67db9eabe7f78972efd7c8251dd
Patrick K <patrick@teamkelso.org>
2014-04-30 20:20:52 +1000

    Thats enough for today

--- FOJ/assignment/Inventory.java
+++ FOJ/assignment/Inventory.java
@@ -2,19 +2,27 @@ import java.util.*;
 public class Inventory
 {
   public Inventory() {
-    products = new ArrayList<ProductLine>();
+    ArrayList<ProductLine> products = new ArrayList<ProductLine>();
+    products.add(new ProductLine("A0001", 10.90, 9.90, 100));
+    products.add(new PerishableProductLine("B0010", 12.00, 7.50, 125, 5));
+    products.add(new PerishableProductLine("C0100", 3.00, 2.30, 1000, 16));
+    products.add(new ItemisedProductLine("D1000", 2600, 2490, 2, "AAA111111"));
+    products.add(new ItemisedProductLine("D1000", 2600, 2490, 2, "BBB222222"));
+    products.add(new ItemisedProductLine("E0001", 699, 509, 3, "CCCC333333"));
+    products.add(new ItemisedProductLine("E0001", 699, 509, 3, "DDDD444444"));
+    products.add(new ItemisedProductLine("E0001", 699, 509, 3, "AAAA222222"));
   }
-    // One possibility for storing Product Lines
-    private ArrayList<ProductLine> products;
+  // One possibility for storing Product Lines
+  private ArrayList<ProductLine> products;
 
-    public void addProduct(ProductLine pl) {
-    }
-    public void sellProduct(String productCode, int no) {
-    }
-    public void increaseStock(String productCode, int no) {
-    }
-    public ProductLine getProduct(String productCode) {
-      return null ;
-    }
+  public void addProduct(ProductLine pl) {
+  }
+  public void sellProduct(String productCode, int no) {
+  }
+  public void increaseStock(String productCode, int no) {
+  }
+  public ProductLine getProduct(String productCode) {
+    return null ;
+  }
 }
 
