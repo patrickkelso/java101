ee3ca8cbecfa76b50aec9be920ed64a55b847467
Patrick K <patrick@teamkelso.org>
2014-05-14 19:55:11 +1000

    moar buttons

--- FOJ/week11/ButtonsPanel.java
+++ FOJ/week11/ButtonsPanel.java
@@ -10,14 +10,44 @@ public class ButtonsPanel extends JPanel {
 
   public ButtonsPanel() {
     setLayout(new FlowLayout());
+
+    AddListener addListener = new AddListener();
+    SubListener subListener = new SubListener();
+    MulListener mulListener = new MulListener();
+    DivListener divListener = new DivListener();
     
     addButton = new JButton("+");
+    addButton.addActionListener(addListener);
     add(addButton);
     addButton = new JButton("-");
+    addButton.addActionListener(subListener);
     add(addButton);
     addButton = new JButton("*");
+    addButton.addActionListener(mulListener);
     add(addButton);
     addButton = new JButton("/");
+    addButton.addActionListener(divListener);
     add(addButton);
+  }
+
+  public class AddListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+      System.out.println("Yay! Plus was clicked");
+    }
+  }
+  public class SubListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+      System.out.println("Yay! Subtract was clicked");
+    }
+  }
+  public class MulListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+      System.out.println("Yay! Multiply was clicked");
+    }
+  }
+  public class DivListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+      System.out.println("Yay! Divide was clicked");
+    }
   }
 }
