92257f65d202cf7b499708504b7f64bfbc3c18d3
Patrick K <patrick@teamkelso.org>
2014-05-14 18:29:52 +1000

    gui

---
+++ FOJ/week11/FirstProgram.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class FirstProgram {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setSize(300, 300);
+    frame.setVisible(true);
+  }
+}
