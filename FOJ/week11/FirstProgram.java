943238178bc2e5c0233e3fbf53de27cddbfef0d1
Patrick K <patrick@teamkelso.org>
2014-05-14 18:41:42 +1000

    moar notes

--- FOJ/week11/FirstProgram.java
+++ FOJ/week11/FirstProgram.java
@@ -5,6 +5,15 @@ public class FirstProgram {
   public static void main(String[] args) {
     JFrame frame = new JFrame();
     frame.setSize(300, 300);
+
+    frame.setLayout(new FlowLayout());
+    frame.add(new JButton("one"));
+    frame.add(new JButton("two"));
+    frame.add(new JButton("three"));
+    frame.add(new JButton("four"));
+
+    frame.pack();
+
     frame.setVisible(true);
   }
 }
